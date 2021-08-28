82925a788a8866457d1c28bddf6fabd25a5abea5
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-08-28 22:12:39 +0530

    Update Animal_Guessing_Game.py

--- Animal_Guessing_Game.py
+++ Animal_Guessing_Game.py
@@ -9,3 +9,25 @@ Features=  [["Small","Brown","Non Mammal"],
             ["Small","Black","Mammal"],
             ["Small","Green","Non Mammal"]]
             
+outcomes=   ["Hen" ,
+            "Dog" ,
+            "Cat", 
+           "Turtle"]
+a=input("Is it Small or Big ? \n")
+b=input("What Colour It is ? \n")
+c=input("Is it Mammal or Non Mammal ? \n")
+if a == "small":
+ print("Ok")
+ if b == "Brown":
+  print("Ok") 
+  if c == "Non Mammal":
+    print("Its a hen")
+  else:
+    print("")
+ else:
+    print("")
+else:
+  print("")  
+ 
+ 
+ 
