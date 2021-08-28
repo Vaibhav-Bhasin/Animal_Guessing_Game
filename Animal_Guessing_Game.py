0e005978e489b58aebcb829f1fcd95508af56ade
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-08-28 22:12:09 +0530

    Update Animal_Guessing_Game.py

--- Animal_Guessing_Game.py
+++ Animal_Guessing_Game.py
@@ -1,1 +1,11 @@
-
+####'''''Topic'''''####
+# Animal Guessing Game 
+ 
+ 
+ 
+print("Animal Guessing Game")
+Features=  [["Small","Brown","Non Mammal"],
+            ["Small","Golden","Mammal"],
+            ["Small","Black","Mammal"],
+            ["Small","Green","Non Mammal"]]
+            
